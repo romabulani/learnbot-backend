5f6ce5842e698dfb42d359feeb727fef48f2e603
Roma Bulani <roma.bulani@vista.com>
2025-01-20 01:11:20 +0530

    add localhost for frontend

--- api/app.py
+++ api/app.py
@@ -102,7 +102,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
